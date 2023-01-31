4e46175171e66b621a1f4d6b788f0c2dbbc4b865
navina321 <81630548+navina321@users.noreply.github.com>
2023-01-31 21:27:03 +0000

    declared variables in main.js file

--- dist/main.dev.js
+++ dist/main.dev.js
@@ -1,3 +1,17 @@
 "use strict";
 
 var _question = require("./question.js");
+
+var question = document.querySelector(".question__text");
+var choices = Array.from(document.querySelectorAll(".question__choices--option"));
+var progressText = document.querySelector(".progress__text");
+var scoreTotal = document.querySelector(".score__total");
+var progressBar = document.querySelector(".progress__bar");
+var currentQuestion = {};
+var acceptAnswers = true;
+var score = 0;
+var questionCount = 0;
+var questionsAvailable = [];
+var questions = _question.questionObject;
+var pointScore = 100;
+var maxQuestions = 20;
--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 import {questionObject} from "./question.js";
+
+const question = document.querySelector(".question__text");
+const choices = Array.from(document.querySelectorAll(".question__choices--option"));
+const progressText = document.querySelector(".progress__text")
+const scoreTotal = document.querySelector(".score__total")
+const progressBar = document.querySelector(".progress__bar")
+
+
+let currentQuestion = {};
+let acceptAnswers = true;
+let score = 0
+let questionCount = 0;
+let questionsAvailable = [];
+let questions = questionObject;
+
+const pointScore = 100;
+const maxQuestions = 20;

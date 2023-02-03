339af8115267043d5b42aea4579e3b812e1b6c63
navina321 <81630548+navina321@users.noreply.github.com>
2023-02-03 14:16:39 +0000

    removed unnessecary files

--- end.js
+++ end.js
@@ -1,4 +1,6 @@
 const finalScore = document.querySelector(".final-score");
 const recentScore = localStorage.getItem("recent-score");
 
+
+
 finalScore.innerText = recentScore;
--- unused/endpage.js
+++ unused/endpage.js
@@ -1,33 +1,33 @@
-const userName = document.querySelector("#userName");
-const saveScore = document.querySelector(".save-score__button");
+// const userName = document.querySelector("#userName");
+// const saveScore = document.querySelector(".save-score__button");
 const finalScore = document.querySelector(".final-score");
 const recentScore = localStorage.getItem("recent-score");
 
-const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+// const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
-const maxStoredScores = 5;
+// const maxStoredScores = 5;
 
 finalScore.innerText = recentScore;
 
-userName.addEventListener("keyup", () => {
-  saveScore.disabled = !userName.value;
-});
+// userName.addEventListener("keyup", () => {
+//   saveScore.disabled = !userName.value;
+// });
 
-const saveHighScore = (e) => {
-  e.preventDefault();
+// const saveHighScore = (e) => {
+//   e.preventDefault();
 
-  const score = {
-    score: recentScore,
-    name: userName.innerText,
-  };
-  highScores.push(score);
+//   const score = {
+//     score: recentScore,
+//     name: userName.innerText,
+//   };
+//   highScores.push(score);
 
-  highScores.sort((a, b) => {
-    return b.score - a.score;
-  });
+//   highScores.sort((a, b) => {
+//     return b.score - a.score;
+//   });
 
-  highScores.splice(5);
+//   highScores.splice(5);
 
-  localStorage.setItem("highScores", JSON.stringify(highScores));
-  window.location.assign("/");
-};
+//   localStorage.setItem("highScores", JSON.stringify(highScores));
+//   window.location.assign("/");
+// };
